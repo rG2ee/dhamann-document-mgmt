9c919efc049d9f6128189a963372cac77c08d755
user <user@123.de>
2026-05-02 08:41:31 +0200

    map betraege

--- src/alle_freelancer_rechnungen/rechnung_constants/rechnungen_2023.py
+++ src/alle_freelancer_rechnungen/rechnung_constants/rechnungen_2023.py
@@ -22,7 +22,6 @@ RECHNUNGEN_2023 = (
     "2023-005-gintech-ag",
 )
 
-
 PATH_2023 = PROJECT_ROOT / "dokumente" / "rechnungen" / "2023"
 
 RECHNUNGEN_2023_F_PATHS = OrderedDict((
@@ -38,21 +37,39 @@ RECHNUNGEN_2023_F_PATHS = OrderedDict((
     ("2023-12-lemonade-research", PATH_2023 / "docu-bot" / "2023-12-lemonade-research-Lemonade_Research_GmbH.pdf"),
 
     # human sign
-    ("2023-001-gintech-ag",  PATH_2023 / "human-sign" / "2023-001-gintech-ag.pdf"),
-    ("2023-002-gintech-ag",  PATH_2023 / "human-sign" / "2023-002-gintech-ag-Gintech_AG.pdf"),
-    ("2023-003-gintech-ag",  PATH_2023 / "human-sign" / "2023-003-gintech-ag-Gintech_AG.pdf"),
-    ("2023-004-gintech-ag",  PATH_2023 / "human-sign" / "2023-004-gintech-ag-Gintech_AG.pdf"),
-    ("2023-005-gintech-ag",  PATH_2023 / "human-sign" / "2023-005-gintech-ag-Gintech_AG.pdf"),
+    ("2023-001-gintech-ag", PATH_2023 / "human-sign" / "2023-001-gintech-ag.pdf"),
+    ("2023-002-gintech-ag", PATH_2023 / "human-sign" / "2023-002-gintech-ag-Gintech_AG.pdf"),
+    ("2023-003-gintech-ag", PATH_2023 / "human-sign" / "2023-003-gintech-ag-Gintech_AG.pdf"),
+    ("2023-004-gintech-ag", PATH_2023 / "human-sign" / "2023-004-gintech-ag-Gintech_AG.pdf"),
+    ("2023-005-gintech-ag", PATH_2023 / "human-sign" / "2023-005-gintech-ag-Gintech_AG.pdf"),
 ))
 
+# rechnung, (betrag: netto, umsatzsteuer)
+RECHNUNGEN_2023_BETRAG_STEUERN = OrderedDict((
 
+    # Pleuger
+    ("2022-12-LR", (7000, 0)),
+    ("2023-01-LR", (5880, 1117.20)),
+
+    # docu bot
+    ("2023-09-lemonade-research", (4880.83, 927.36)),
+    ("2023-10-lemonade-research", (4907.50, 932.43)),
+    ("2023-11-lemonade-research", (2935.00, 557.65)),
+    ("2023-12-lemonade-research", (4582.50, 870.68)),
+
+    # human sign
+    ("2023-001-gintech-ag", (2272.00, 0.00)),
+    ("2023-002-gintech-ag", (5082.00, 0.00)),
+    ("2023-003-gintech-ag", (5314.00, 0.00)),
+    ("2023-004-gintech-ag", (4950.00, 0.00)),
+    ("2023-005-gintech-ag", (2866.00, 0.00)),
+))
 
 import os
+
 if __name__ == '__main__':
     print(len(RECHNUNGEN_2023_F_PATHS))
 
     for key, val in RECHNUNGEN_2023_F_PATHS.items():
         print(val)
         print(os.path.isfile(val))
-
-
