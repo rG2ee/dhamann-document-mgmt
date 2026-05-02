1d5e89a0b54acf2af9f41304e3bb76b6d3489abe
user <user@123.de>
2026-05-02 08:43:50 +0200

    init

---
+++ src/alle_freelancer_rechnungen/rechnung_constants/rechnungen_2024.py
@@ -0,0 +1,9 @@
+from collections import OrderedDict
+
+from alle_freelancer_rechnungen.constants import PROJECT_ROOT
+
+RECHNUNGEN_2024 = (
+
+)
+
+PATH_2023 = PROJECT_ROOT / "dokumente" / "rechnungen" / "2024"
