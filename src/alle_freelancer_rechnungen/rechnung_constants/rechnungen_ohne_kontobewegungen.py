24ce917cc5f1f7bfdbababece05605d7e18569cc
user <user@123.de>
2026-05-03 10:46:21 +0200

    add RECHNUNGEN_OHNE_KONTOBEWEGUNGEN

---
+++ src/alle_freelancer_rechnungen/rechnung_constants/rechnungen_ohne_kontobewegungen.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class RechnungOhneKontobewegung(BaseModel):
+    rechnung_nr: str
+    grund: str
+
+
+RECHNUNGEN_OHNE_KONTOBEWEGUNGEN: list[RechnungOhneKontobewegung] = [
+    RechnungOhneKontobewegung(rechnung_nr="2024-6-kdw", grund="verrechnet mit 2024-6-kdw-korrektur"),
+    RechnungOhneKontobewegung(rechnung_nr="2024-6-kdw-korrektur", grund="verrechnet mit 2024-6-kdw"),
+    RechnungOhneKontobewegung(rechnung_nr="2024-8-kdw", grund="umgezogen auf newco 2024-8-kadewe"),
+    RechnungOhneKontobewegung(rechnung_nr="2026-01-kadewe-r2", grund="letzte woche gestellt"),
+    RechnungOhneKontobewegung(rechnung_nr="2026-03-kadewe", grund="letzte woche gestellt"),
+    RechnungOhneKontobewegung(rechnung_nr="2026-04-allgeier", grund="letzte woche gestellt"),
+    RechnungOhneKontobewegung(rechnung_nr="2026-02-kadewe", grund="letzte woche gestellt"),
+    RechnungOhneKontobewegung(rechnung_nr="2026-01-kadewe-r1", grund="letzte woche gestellt"),
+    RechnungOhneKontobewegung(rechnung_nr="2026-03-allgeier", grund="am 30.04 bezahlt, aber noch nicht im export"),
+]
