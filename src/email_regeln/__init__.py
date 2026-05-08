d008278a9beed4c5ae9d09dee2be5f69d7d6ac24
user <user@123.de>
2026-05-08 22:41:59 +0200

    next execution after manual reviews

---
+++ src/email_regeln/__init__.py
@@ -0,0 +1,541 @@
+
+"""
+Host
+-------------------------------------------------------------
+deltakonnect.de      --eigener ordner
+ramona-mertens.com  --eigener ordner
+gmail.com                           --eigener ordner
+protonmail.com                                      --eigener ordner
+t-online.de --eigener ordner
+booking.com   --eigener ordner
+chess.com     --eigener ordner
+stripe.com    --ignore
+gintech.io  --eigener ordner
+urvin.finance   -> alles löschen
+mail.anthropic.com     --eigener ordner
+gluecksburg.shbb.de    -> steuer
+google.com    --eigener ordner
+gmx.de  --ignore
+builder.io  --eigener ordner
+xceed.me
+neurologie-neuer-wall.de -gesundheit
+pinecone.io  --ignorierien
+debeka.de   --gesundheit
+lemonade-research.de  --cubicl
+ifttt.com  --ignorierien
+airbnb.com    --eigener ordner
+info.debeka.de --gesundheit
+email.hays.com --ignorierien
+boomfestival.org          --eigener ordner
+service.eventim.de --eigener ordner
+etke.host --eigener ordner
+soorce.de   --eigener ordner
+supabase.com --eigener ordner
+smartflow-consulting.com
+hungry4.io
+mail.coinbase.com
+fem-ai.com   --eigener ordner
+crissbellini.com
+computershare online services
+service.alibaba.com
+e.opodo.com
+jameda.de --  --eigener ordner
+vattenfall.de   --eigener ordner
+eventmail.flatex.de
+orgainic.com
+notifications.soundcloud.com --eigener ordner
+agoda.com   --eigener ordner
+nymtech.net --eigener ordner
+mail.yfood.eu --delete
+selber-lagern.de --eigener ordner
+mailing.milesandmore.com --eigener ordner
+figma.com  --eigener ordner
+mediamarkt.de  --eigener ordner
+ikea.com
+ups.com
+m.ngrok.com
+mkt.vorwerk.com
+nemensis.de
+newsletter.zattoo.com
+dfinity.org
+duolingo.com
+facebookmail.com
+b.12go.asia
+pal-tv.de
+dampfdorado.de
+research.fiverr.com
+versand-status.de
+mail.instagram.com
+wolt.com
+reply.em.home24.de
+info.emmy-sharing.de
+updates.fresha.com
+solcom.de
+bassliner.org
+skatbank.de
+tuhh.de
+mailing.amorelie.de
+welcome.americanexpress.com
+e.battle.net
+uber.com
+proton.me
+news.mieterverein-hamburg.de
+hostelworld.com
+trip.com
+finanzguru.de
+info.wise.com
+patreon.com
+mailing.tivoli.de
+hauer-naturprodukte.com
+redglobal.com
+email.anthropic.com
+discord.com
+login.soundcloud.com
+meetup.com
+wasabi.com
+order.email.ikea.com
+motelamiio.com
+ebay.com
+termine.hamburg.de
+klarna.de
+shippypro.com
+hamburg.de
+mindfactory.de
+information.swiss.com
+zara.com
+redditmail.com
+builderio.zendesk.com
+welcome.moia-mail.io
+deutschebahn.com
+notifications.galaxus.de
+thomann.de
+ko-fi.com
+ownly.de
+reply.gulp.de
+thryvetalent.com
+edigital.de
+tricentis.com
+notify.proton.me
+info.tapmilesandgo.com
+infos.doctolib.com
+taures.de
+developer.etebase.com
+traderepublic.com
+eg.expedia.com
+hays.de
+n.refurbed.com
+reply.flatex.de
+
+
+sunexpress.com
+realvnc.com
+pleugerindustries.com
+m.email.hostelworld.com
+property.booking.com
+shore.com
+rply.flatex.de
+orthopädikum
+email.harveynash.com
+notification.miro.com
+eventim.de
+activitar.com
+huggingface.co
+flytap.com
+your.lufthansa-group.com
+mail.wolt.com
+email.remarkable.com
+delivery.hm.com
+cpucommunications.com
+kaufland-marktplatz.de
+powherbeat.com
+skribble.com
+microsoft account team
+cubicl.de
+kopfundsteine.de
+janado.de
+paylogic.com
+email.wandb.com
+callabike-interaktiv.de
+trakt.tv
+e-mail.joyn.de
+go.storj.io
+real-debrid.com
+service.easybank.de
+news.doctolib.com
+kagi by with reach
+    cookidoo.vorwerk.com
+mail.free-now.com
+mountainduck.io
+mail.miro.com
+digitalassociates.de
+mail.cursor.com
+cccv.de
+gymmasteronline.com
+information.lufthansa.com
+bitwarden.com
+gigabyte.com
+novafind.eu
+dsit.de
+mail.my.mediamarkt.de
+galaxus.de
+hamburgopenatp500.com
+letsencrypt.org
+support.soundcloud.com
+fedex.com
+m.maskworld.com
+t.shopifyemail.com
+blackroll.com
+amorelie.com
+payment.moia-mail.io
+zenlap.com
+outlook.de
+twitter.com
+tixforgigs.com
+immobilienscout24.de
+email.prioritypass.com
+interactivebrokers.ie
+modemfestival.com
+coworkbooking.com
+link.com
+buyzoxs.de
+playchessup.com
+external.kadewe.com
+stadtradhamburg.de
+audible.com
+sixt.com
+feedbackemail.americanexpress.com
+h6.hilton.com
+survey.flytap.com
+my-notification.flytap.com
+ali
+de.nespresso.com
+interactive brokers ireland limited
+zulip.futo.org
+noreply.market.home24.de
+mail.hinge.co
+maskworld.com
+opentable.de
+email.alibaba.com
+matratzen-concord.de
+campingwagner.de
+external.kadewegroup.com
+digichance.eu
+wandb.com
+ngrok.com
+macincloud.com
+notice.alibaba.com
+accounts.expedia.com
+pvs-se.de
+streber24.de
+zoxs.de
+justcom.de
+netcup.de
+postbank.de
+hermes
+sita.aero
+x.ai
+id.atlassian.com
+elbphilharmonie laeiszhalle hamburg
+comspot.repair
+joplincloud.com
+moyn-ticket.de
+accounts.dev
+valyue.de
+caseking.de
+e.ray-ban.com
+info.coinbase.com
+cloudbeds.com
+eu1.storj.io
+gamma.app
+email-marriott.com
+documentsgpt.com
+hinge.co
+security.galaxus.de
+news.de.shein.com
+wetransfer.com
+vaporexmachina.de
+jsanconsulting.com
+xcover.com
+alerts.oknotify3.com
+pm.me
+sendcloud.com
+skins4u.de
+goflink.com
+cblank.de
+canva.com
+ruggell.li
+email.etsy.com
+services.decathlon.de
+creatorkit.com
+de.expediamail.com
+sthree.com
+celona.com
+mail.gorillasapp.de
+builder.dontvacuum.me
+reddit.com
+mhpatent.de
+manuelhehle.com
+laion.ai
+mitgliederverwaltung.fitness
+kalo-hamburg.de
+the-ascott.com
+antares-apotheke hohe weide via no-q digital customer queuein
+emirates.com
+hermes zustellbenachrichtigung
+accounts.emirates.email
+email.doctolib.com
+info.hostelworld.com
+fusehostelsandtravel.com
+updates.oknotify3.com
+5612126.brevosend.com
+eventim-light.com
+lichess.org
+ing.de
+groq.com
+account.traderepublic.com
+express.medallia.com
+audible.de
+vicoach.io
+recovery.proton.me
+de
+boardingpass.lufthansa.com
+link2extreme.com
+qemailserver.com
+paygenius.co.za
+boardingpass.swiss.com
+upgradebargain.swiss.com
+communication.microsoft.com
+zepp.com
+orthopaedie-elsterplatz.de
+sanparks.org
+multisafepay.com
+mailing.rewe.de
+serverschrank24.de
+info.flytap.com
+connect.lieferando.de
+haspa.de
+rezeptwelt.de
+email.meetup.com
+deiters.de
+insolvenzschutz-mail.drsf.reise
+ds_e-sales_gmbh | mc4zw3q
+wootric.com
+tapmilesandgo.com
+info.ebay.de
+information.milesandmore.com
+source-technology.com
+audioplay-events.com
+ruby-hotels.com
+everfit-mail.com
+bvg.de
+tivoli.de
+texttodesign.ai
+physio-lutterothstrasse.de
+emmy-sharing.de
+e.blizzard.com
+trp.tmecosys.com
+vorwerk.de
+member.alibaba.com
+outlook.com
+opentable | go by steffen henssler hamburg reservierung
+amoriabond.com
+estelleyoga.com
+account.miro.com
+trustedshops.com
+pozngo.com
+tunemymusic.com
+partners.decathlon.com
+feedback.decathlon.com
+lexoffice.de
+ticketswap.com
+enterprise.com
+patrick gordon | elbeit consult gmbh
+delivery-status.com
+laruche-logistique.fr
+yfood.eu
+battle.net
+refeedigital.com
+foxmail.com
+expedia.com
+eversports.com
+constaff.com
+mindsquare.de
+team-hy.de
+gelenketesch.de
+web.de
+email.patreon.com
+bahn.de
+mailing.mgs-eportal.de
+terminland.de
+alternate.de
+infos.joyn.de
+swapzone.io
+stakingrewards.com
+notifications.huggingface.co
+norsan.de
+biogena.at
+biogena.com
+youtube.com
+serpapi.com
+yahoo.de
+etesync.com
+apotheke am hauptbahnhof via no-q digital customer queueing
+rathaus-apotheke via no-q digital customer queueing
+wildpark schwarze berge online shop
+brasil.tomorrowland.com
+confirmation.all.com
+hermes benachrichtigungsfunktion
+belege.lexware.de
+mail.notion.so
+care.emirates.email
+callabike.de
+leonard zimmermann
+winter.tomorrowland.com
+mail.audible.com
+tdacservices.immigration.go.th
+12go.asia
+blueribbonbags.com
+news.xceed.me
+jetbrains.com
+allgeier-itc.ch
+mail.postman.com
+mue-thaimassage.de
+insideapple.apple.com
+kadewe.de
+fathom.video
+t.email.hostelworld.com
+abogado
+email.alltrails.com
+sport.decathlon.de
+info.thetrainline.com
+email.claude.com
+e-mail.xing.com
+product.miro.com
+h5.hilton.com
+trip
+email-support.airbnb.com
+prosperalink-7.com
+luxotticaretail.com
+updates.notion.so
+rscw.de
+flyedelweiss.com
+7pass.de
+mieterjournal.de
+email-support.uber.com
+gpodder.net
+ionos.de
+email.hm.com
+venturi-group.com
+noti.swiss.com
+yolawo.de
+brains-work.de
+corp.sixt.de
+telegram.org
+✅
+contact.nespresso.com
+michael
+mieterverein-newsletter.de
+autoemail.hm.com
+alerts.spotify.com
+fireflies.ai
+info.email.ikea.com
+cm.order.email.ikea.com
+encarnação
+tisso.de
+myflexbox.de
+smile.austrian.com
+apcoa.com
+inspire.pinterest.com
+klarna.gr
+vorwerk.com
+futo.org
+thermomix-rezeptwelt.maileon.de
+larsglobisch.com
+elbphilharmonie.de
+info.riverty.com
+email2.anthropic.com
+cursor.sh
+progressiverecruitment.com
+bryghtlabs.com
+wger.de
+koe-hair.de
+promo.istyle.id
+krimi-trails.de
+crowdcontrol.co
+feedback-ekomi.com
+axitera.de
+mail.cronometer.com
+cyprec.com
+eventix.nl
+hva-systemes.com
+sc-fahrpreisnacherhebung.de
+dhl.com
+e.miro.com
+db-fn.de
+buynotice.alibaba.com
+evy.eu
+vodafone.com
+service.decathlon.com
+refurbed service
+messages.xoom.com
+cronometer.com
+service.refurbed.com
+pentrovill.de
+servicepaket93.de
+profacts.eu
+sushifuerhamburg.com
+iterate.ch
+sheinemail.com
+progressive.de
+managilityltd.com
+bengalurologicalsociety.org
+infomail.microsoft.com
+notificationmail.microsoft.com
+ideas.pinterest.com
+notifications.microsoft.com
+accountprotection.microsoft.com
+info.filebase.com
+mail.beehiiv.com
+haffer.at
+sg.booking.com
+susanne-friedrich.com
+tiuldi.createsend7.com
+boardlab.de
+mint
+th.expediamail.com
+tomorrowland.com
+koh
+mund-consulting.de
+wabcon.de
+pcs-it.at
+p
+gulp.de
+dev-x.tech
+joyit-berlin.de
+lawrenceharvey.com
+app.debeka.de
+icloud.com
+pca.levillagebyca.com
+bookandplay.de
+whitebit.promo
+spectrumemails.com
+ra.co
+x.com
+cc.magicline.com
+pass.proton.me
+freistetter.org
+discover.pinterest.com
+forefront.ai
+mail.openai.com
+cuisine.rudirocks.com
+service.emmy-sharing.de
+lenovo.com
+mail.getoutline.com
+buy me a coffee
+wir
+hkl24.com
+creator.patreon.com
+campaign.eventbrite.com
+-------------------------------------------------------------
+TOTAL
+"""
